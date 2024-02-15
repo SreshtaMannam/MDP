fcf1cdee0d794bbb64e326c0704331e8ded862a8
satwikasuru <95873202+satwikasuru@users.noreply.github.com>
2024-02-15 19:55:29 +0530

    Update DiseasePredictionSystem.py

--- DiseasePredictionSystem.py
+++ DiseasePredictionSystem.py
@@ -5,6 +5,16 @@ import pickle
 import streamlit as st
 from streamlit_option_menu import option_menu
 
+from PIL import Image
+
+icon = Image.open('page_icon.png')
+st.set_page_config(
+        page_title="Multiple Disease Prediction",
+        page_icon=icon,
+        layout="wide"
+)
+
+
 #loading the saved models
 diabetes_model = pickle.load(open('diabetes_model.sav', 'rb'))
 
@@ -12,15 +22,19 @@ heart_disease_model = pickle.load(open('heart_disease_model.sav', 'rb'))
 
 parkinsons_model = pickle.load(open('parkinsons_model.sav', 'rb'))
 
+kidney_disease_model = pickle.load(open('kidney_model.sav', 'rb'))
+
+
 #sidebar for navigation
 with st.sidebar:
-    select = option_menu('Multiple Disease Prediction',
+    select = option_menu('Diseases Prediction',
                          
                          ['Diabetes Prediction',
                           'Heart Disease Prediction',
-                          'Parkinsons Disease Prediction'],
+                          'Parkinsons Disease Prediction',
+                          'Kidney Disease Prediction'],
                          
-                         icons = ['activity', 'heart', 'person'],
+                         icons = ['activity', 'heart', 'person', ''],
                          
                          default_index = 0)
     
@@ -76,9 +90,29 @@ if select == 'Heart Disease Prediction':
     with col1:
         age = st.text_input('Age')
     with col2:
-        sex = st.text_input('Sex')
+        sex=0
+        display = ("Select", "Male", "Female")
+        options = list(range(len(display)))
+        value = st.selectbox("Gender", options, format_func=lambda x: display[x])
+        if value == "Male":
+            sex = 1
+        elif value == "Female":
+            sex = 0
+        
     with col3:
-        cp = st.text_input('Chest Pain type')
+        cp=0
+        display = ("Select", "Typical Angina", "Atypical Angina", "Non-Anginal Pain", "Asymptotic")
+        options = list(range(len(display)))
+        value = st.selectbox("Chest_Pain Type", options, format_func=lambda x: display[x])
+        if value == "Typical Angina":
+            cp = 0
+        elif value == "Atypical Angina":
+            cp = 1
+        elif value == "Non-Anginal Pain":
+            cp = 2
+        elif value == "Asymptotic":
+            cp = 3
+        
     with col1:
         trestbps = st.text_input('Resting Blood Pressure')
     with col2:
@@ -182,6 +216,83 @@ if select == 'Parkinsons Disease Prediction':
             parkinsons_diagnosis = 'The person Has Parkinsons Disease'
             
     st.success(parkinsons_diagnosis)
+    
+#Kidney Disease prediction page
+if select == 'Kidney Disease Prediction':
+    #page title
+    st.title('Kidney Disease Prediction')
+    
+    #columns for input fields
+    col1, col2, col3 = st.columns(3)
+    
+
+    
+    with col1:
+        age = st.text_input('Age')
+    with col2:
+        bp = st.text_input('Blood Pressure') 
+    with col3:
+        sg = st.text_input('Specific Gravity')        
+    with col1:
+        al = st.text_input('Albumin')
+    with col2:
+        su = st.text_input('Sugar Level')
+    with col3:
+        rbc = st.text_input('Red Blood Cells')
+    with col1:
+        pc = st.text_input('Pus Cells')
+    with col2:
+        pcc = st.text_input('Pus Cell Clumps')
+    with col3:
+        ba = st.text_input('Bacteria')
+    with col1:
+        bgr = st.text_input('Blood Glucose Random')
+    with col2:
+        bu = st.text_input('Blood Urea')
+    with col3:
+        sc = st.text_input('Serum Creatinine')        
+    with col1:
+        sod = st.text_input('Sodium')
+    with col2:
+        pot = st.text_input('Potassium')
+    with col3:
+        hemo = st.text_input('Hemoglobin')
+    with col1:
+        pcv = st.text_input('Packed Cell Volume')
+    with col2:
+        wc = st.text_input('White Blood Cell Count')
+    with col3:
+        rc = st.text_input('Red Blood Cell Count')
+    with col1:
+        htn = st.text_input('Hypertension')
+    with col2:
+        dm = st.text_input('Diabetes Mellitus')
+    with col3:
+        cad = st.text_input('Coronary Artery Disease')
+    with col1:
+        appet = st.text_input('Appetite')        
+    with col2:
+        pe = st.text_input('Pedal Edema')
+    with col3:
+        ane = st.text_input('Anemia')
+    
+    
+    
+    #code for Prediction
+    kidney_diagnosis = ''
+    
+    #button for prediction
+    if st.button('Kidney Disease Test Result'):
+        kidney_prediction = kidney_disease_model.predict([[age, bp, sg, al, su, rbc, pc, pcc, ba, bgr, bu, sc, sod, pot, hemo, pcv, wc, rc, htn, dm, cad, appet, pe, ane]])
+        
+        if kidney_prediction[0]==0:
+            kidney_diagnosis = 'The person does Not Have Kidney Disease'
+        else:
+            kidney_diagnosis = 'The person Has Kidney Disease'
+            
+    st.success(kidney_diagnosis)
+
+
     
 
 
