a2548f327c35a0b71af4baf69cae148f2e89dec0
Sreshta_Mannam <93534609+SreshtaMannam@users.noreply.github.com>
2024-03-16 16:06:05 +0530

    Update DiseasePredictionSystem.py

--- DiseasePredictionSystem.py
+++ DiseasePredictionSystem.py
@@ -77,77 +77,73 @@ if (selected == 'Diabetes Prediction'):
     st.success(diab_diagnosis)
 
 
-
-#Heart Disease prediction page
+# Heart Disease Prediction Page
 if selected == 'Heart Disease Prediction':
-    #page title
-    st.title('Heart Disease Prediction')
-    
-    #columns for input fields
+
+    # page title
+    st.title('Heart Disease Prediction using ML')
+
     col1, col2, col3 = st.columns(3)
-    
+
     with col1:
         age = st.text_input('Age')
+
     with col2:
-        sex=0
-        display = ("Select", "Male", "Female")
-        options = list(range(len(display)))
-        value = st.selectbox("Gender", options, format_func=lambda x: display[x])
-        if value == "Male":
-            sex = 1
-        elif value == "Female":
-            sex = 0
-        
+        sex = st.text_input('Sex')
+
     with col3:
-        cp=0
-        display = ("Select", "Typical Angina", "Atypical Angina", "Non-Anginal Pain", "Asymptotic")
-        options = list(range(len(display)))
-        value = st.selectbox("Chest_Pain Type", options, format_func=lambda x: display[x])
-        if value == "Typical Angina":
-            cp = 0
-        elif value == "Atypical Angina":
-            cp = 1
-        elif value == "Non-Anginal Pain":
-            cp = 2
-        elif value == "Asymptotic":
-            cp = 3
-        
+        cp = st.text_input('Chest Pain types')
+
     with col1:
         trestbps = st.text_input('Resting Blood Pressure')
+
     with col2:
-        chol = st.text_input('Serum Cholestrol in mg/dL')
+        chol = st.text_input('Serum Cholestoral in mg/dl')
+
     with col3:
-        fbs = st.text_input('Fasting Blood Sugar > 120 mg/dL')
+        fbs = st.text_input('Fasting Blood Sugar > 120 mg/dl')
+
     with col1:
         restecg = st.text_input('Resting Electrocardiographic results')
+
     with col2:
         thalach = st.text_input('Maximum Heart Rate achieved')
+
     with col3:
         exang = st.text_input('Exercise Induced Angina')
+
     with col1:
         oldpeak = st.text_input('ST depression induced by exercise')
+
     with col2:
         slope = st.text_input('Slope of the peak exercise ST segment')
+
     with col3:
         ca = st.text_input('Major vessels colored by flourosopy')
+
     with col1:
         thal = st.text_input('thal: 0 = normal; 1 = fixed defect; 2 = reversable defect')
-    
-    
-    #code for Prediction
-    heart_disease_diagnosis = ''
-    
-    #button for prediction
+
+    # code for Prediction
+    heart_diagnosis = ''
+
+    # creating a button for Prediction
+
     if st.button('Heart Disease Test Result'):
-        heart_disease_prediction = heart_disease_model.predict([[age, sex, cp, trestbps, chol, fbs, restecg, thalach, exang, oldpeak, slope, ca, thal]])
-        
-        if heart_disease_prediction[0]==0:
-            heart_disease_diagnosis = 'The person does Not Have Heart Disesase'
+
+        user_input = [age, sex, cp, trestbps, chol, fbs, restecg, thalach, exang, oldpeak, slope, ca, thal]
+
+        user_input = [float(x) for x in user_input]
+
+        heart_prediction = heart_disease_model.predict([user_input])
+
+        if heart_prediction[0] == 1:
+            heart_diagnosis = 'The person is having heart disease'
         else:
-            heart_disease_diagnosis = 'The person Has Heart Disease'
-            
-    st.success(heart_disease_diagnosis)
-        
+            heart_diagnosis = 'The person does not have any heart disease'
+
+    st.success(heart_diagnosis)
+
     
     
 
