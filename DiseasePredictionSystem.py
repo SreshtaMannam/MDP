7584789caf2e62fb653d453ca42533b21da468ab
Sreshta_Mannam <93534609+SreshtaMannam@users.noreply.github.com>
2024-03-20 23:06:37 +0530

    Update DiseasePredictionSystem.py

--- DiseasePredictionSystem.py
+++ DiseasePredictionSystem.py
@@ -73,8 +73,8 @@ if (selected == 'Diabetes Prediction'):
         if (diab_prediction[0] == 1):
             st.image("https://en.pimg.jp/053/159/012/1/53159012.jpg",  width=300)
             diab_diagnosis = 'The person is diabetic'
-            st.markdown("[Download PDF](https://www.cdc.gov/diabetes/ndep/pdfs/4steps/4steps-english.pdf)", unsafe_allow_html=True)
-            diab_diagnosis += " Diabetes, a chronic condition characterized by elevated blood sugar levels, has no definitive cure. Treatment primarily focuses on managing blood sugar levels to prevent complications.Proper management often involves a combination of medication, lifestyle changes, and regular monitoring of blood glucose levels to keep them within target ranges."
+            st.markdown("[Your Guide2Diabetes](https://www.cdc.gov/diabetes/ndep/pdfs/4steps/4steps-english.pdf)", unsafe_allow_html=True)
+            diab_diagnosis += "Proper management often involves a combination of medication, lifestyle changes, and regular monitoring of blood glucose levels to keep them within target ranges."
         else:
             st.image("https://thumbs.dreamstime.com/b/negative-premium-soft-green-round-button-isolated-abstract-illustration-105963957.jpg",  width=300)
             diab_diagnosis = 'The person is not diabetic'
